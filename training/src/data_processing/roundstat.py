3e4a4d68ac4122009b54ec8cefc33a7d16a73e8a
leminhohoho <leminhnguyenai@gmail.com>
2025-09-04 16:38:31 +0000

    Delete old training script

--- training/src/data_processing/roundstat.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import os
-import pandas as pd
-
-
-def get_rounds_stats(
-    conn: sqlite3.Connection, match_id: int, map_id: int, team_id: int
-):
-    rounds_stats = load_map_rounds_stats(conn, match_id, map_id, team_id)
